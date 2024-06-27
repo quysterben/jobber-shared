cdae0038034fd7841c17e87db5177165af42a2d0
quy.hoang <quy.hoang@kiaisoft.com.vn>
2024-06-27 16:02:14 +0700

    feature/add-interfaces-and-cloudinary-upload

---
+++ src/index.ts
@@ -0,0 +1,82 @@
+export {
+  IAuthPayload,
+  IAuthDocument,
+  IAuthBuyerMessageDetails,
+  IEmailMessageDetails,
+  IAuth,
+  ISignUpPayload,
+  ISignInPayload,
+  IForgotPassword,
+  IResetPassword,
+  IReduxAuthPayload,
+  IReduxAddAuthUser,
+  IReduxLogout,
+  IAuthResponse,
+  IAuthUser,
+} from './interfaces/auth.interface';
+
+export { IBuyerDocument, IReduxBuyer } from './interfaces/buyer.interface';
+
+export {
+  IConversationDocument,
+  IMessageDocument,
+  IMessageDetails,
+  IChatBoxProps,
+  IChatSellerProps,
+  IChatBuyerProps,
+  IChatMessageProps,
+} from './interfaces/chat.interface';
+
+export {
+  GigType,
+  ICreateGig,
+  ISellerGig,
+  IGigContext,
+  IGigsProps,
+  IGigCardItems,
+  ISelectedBudget,
+  IGigViewReviewsProps,
+  IGigInfo,
+  IGigTopProps,
+} from './interfaces/gig.interface';
+
+export {
+  IOrderNotifcation,
+  IOffer,
+  IExtendedDelivery,
+  IDeliveredWork,
+  IOrderEvents,
+  IOrderReview,
+  IOrderMessage,
+  IOrderDocument,
+} from './interfaces/order.interface';
+
+export {
+  IReviewMessageDetails,
+  IRatingTypes,
+  IRatingCategories,
+  IRatingCategoryItem,
+  IReviewDocument,
+} from './interfaces/review.interface';
+
+export {
+  ISearchResult,
+  IHitsTotal,
+  IQueryList,
+  IQueryString,
+  ITerm,
+  IPaginateProps,
+} from './interfaces/search.interface';
+
+export {
+  SellerType,
+  ILanguage,
+  IExperience,
+  IEducation,
+  ICertificate,
+  ISellerDocument,
+} from './interfaces/seller.interface';
+
+export { IEmailLocals } from './interfaces/email.interface';
+
+export {uploads, videoUpload} from './cloudinary-upload';
